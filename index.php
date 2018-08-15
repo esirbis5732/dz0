c86c29ef465ca60decd832615a337871a17385ee
Sergei Elizarov <ser.elizarow2015@yandex.ru>
2018-08-16 00:24:52 +0300

    привел к 'PSR-2'

--- index.php
+++ index.php
@@ -9,6 +9,11 @@
 CONST testConstanta = 'asd';
 $user_name = "Igor";
 
-function show_something(){ RETURN 'SOMETHING';}
+function show_something()
+{
+	RETURN 'SOMETHING';
+}
 
-if (1==1) echo 'hi';
+if (1 == 1) {
+	echo 'hi';
+}
