1b6147525346eb6a0d13715f4587ac289bcff4cd
Sergei Elizarov <ser.elizarow2015@yandex.ru>
2018-08-15 23:32:50 +0300

    начало

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nadin
+ * Date: 15.08.2018
+ * Time: 23:14
+ */
+
+CONST testConstanta = 'asd';
+$user_name = "Igor";
+
+function show_something(){ RETURN 'SOMETHING';}
+
+if (1==1) echo 'hi';
